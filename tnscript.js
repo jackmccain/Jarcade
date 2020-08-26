dc46a7d4607b8154577b7b6f8c9aa24650f2be1e
jackmccain <66799013+jackmccain@users.noreply.github.com>
2020-08-26 01:59:36 -0400

    Homepage
    Co-authored-by: jmcgarry@mac.com <jmcgarry@mac.com>

--- tnscript.js
+++ tnscript.js
@@ -1,5 +1,27 @@
 var character = document.getElementById("character");
 var side = true;
+var scoreElapsed = 0;
+var score = 0;
+
+function onGameStart(){
+    var gameStatus = true
+    start();
+}
+function onGameEnd(){
+    gameStatus = false
+    stop();
+}
+
+function start() {
+    score = setInterval(function(){
+        scoreElapsed += 1;
+         document.getElementById("time").innerText = scoreElapsed;
+    }, 10) ;
+
+}
+function stop() {
+    clearInterval(score);
+}
 
 function jump() {
     if (side === true) {
@@ -27,22 +49,24 @@ function jumpright() {
         character.style.backgroundImage = "url('wall.png')";
         character.style.left = "80px";
         side = true;
-        character.classList.remove("animate")
+        character.classList.remove("animate");
     }
 }
 
-var branchright = document.getElementById("branchright");
-var branchleft = document.getElementById("branchleft");
+var branchright;
+var branchleft;
 var random;
 
 setInterval(function branch() {
+  
     random = Math.floor(Math.random() * 2) + 1;
+    console.log(random)
     if (random === 1) {
-        branchright.classList.add("branchanimate")
+        document.getElementById("branchright").classList.add("branchanimate");
     } else {
-        branchleft.classList.add("branchanimate")
+        document.getElementById("branchleft").classList.add("branchanimate");
     }
 
 }, 3000);
-
+onGameStart();
 branch();
